c6e9941bc6c8c0b411c593b127509d8adedc7161
betoswag <76641802+betoswag@users.noreply.github.com>
2021-01-19 15:46:29 -0300

    Add files via upload

---
+++ help.js
@@ -0,0 +1,67 @@
+const help = (prefix) => {
+	return `
+
+╔════════════════════
+║        🌹DOKA BOT🌹
+╠════════════════════
+
+
+➸ Prefix:  *「${prefix} 」*
+➸ Status: *「 Online 」*
+
+       • ──── ✾ ──── •
+       *FIGURINHAS*【✔】
+       • ──── ✾ ──── •
+      
+➸ Comando : *${prefix}sticker* ou *${prefix}stiker*
+➸ útil em : converter imagem/gif/vídeo em adesivo
+➸ uso : responder imagem/gif/video ou enviar imagem/gif/video com legenda\n
+
+➸ Comando : *${prefix}sticker nobg* ou *${prefix}stiker nobg*
+➸ útil em : converter imagem em adesivo removendo o fundo
+➸ uso : responder imagem ou enviar imagem com legenda/n
+
+➸ Comando : *${prefix}toimg*
+➸ útil em : converter adesivo em imagem
+➸ uso : adesivo de resposta\n
+
+➸ Comando : *${prefix}tsticker* ou *${prefix}tstiker*
+➸ útil em : converter texto em adesivo
+➸ uso : *${prefix}tsticker seu texto aqui*\n
+
+       • ─── ✾ ─── •
+       *MEMES*【✔】
+       • ─── ✾ ─── •
+      
+➸ Comando : *${prefix}meme*
+➸ útil em : mandar imagens aleatórias de meme [inglês]
+➸ uso : basta emviar o comando\n
+➸ Comando : *${prefix}memeindo*
+➸ útil em : mandar imagens aleatórias de meme [indo]
+➸ uso : basta enviar o comando
+
+       • ──── ✾ ──── •
+       *OUTROS...*【✔】
+       • ──── ✾ ──── •
+      
+➸ Comando : *${prefix}gtts*
+➸ útil em : converter texto em fala/áudio
+➸ uso : *${prefix}gtts [cc] [text]*\nexemplo : *${prefix}gtts ja On2-chan*\n
+
+➸ Comando : *${prefix}ocr*
+➸ útil em : pegar o texto da foto e lhe enviar
+➸ uso : responder imagem ou enviar mensagem com legenda\n
+
+       • ────── ✾ ────── •
+       *MENU DO BETO*【✔】
+       • ────── ✾ ────── •      `
+}
+
+exports.help = help
+
+
+
+
+
+
+
